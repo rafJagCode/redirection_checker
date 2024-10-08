409294240d11759ee7969f2f3f07a43f276cce84
Rafał Jagielski <rjagielski@nabthat.com>
2024-10-08 17:02:37 +0200

    Change to synchronous link checking

--- index.js
+++ index.js
@@ -3,23 +3,19 @@ import { parse } from "csv-parse";
 import { checkRedirection } from "./checkRedirection.js";
 
 const linkMap = new Map();
-const promises = [];
 
 fs.createReadStream("./links-in.csv")
   .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", (row) => {
     const link = row[0];
     linkMap.set(link, null);
-    const promise = checkRedirection(link).then((redirectedTo) => {
-      linkMap.set(link, redirectedTo);
-    });
-    promises.push(promise);
   })
-  .on("end", () => {
-    Promise.all(promises).then(() => {
-      const output = fs.createWriteStream("./links-out.csv");
-      output.write("LINKS OUT\n");
-      for (const [_, redirectedTo] of linkMap)
-        output.write(`${redirectedTo}\n`);
-    });
+  .on("end", async () => {
+    for (const [link, _] of linkMap) {
+      const redirectTo = await checkRedirection(link);
+      linkMap.set(link, redirectTo);
+    }
+    const output = fs.createWriteStream("./links-out.csv");
+    output.write("LINKS OUT\n");
+    for (const [_, redirectedTo] of linkMap) output.write(`${redirectedTo}\n`);
   });
